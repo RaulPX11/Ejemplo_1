060e7c303edf567970453a1c5d6e5beec554010f
RaulPX11 <99362109+RaulPX11@users.noreply.github.com>
2022-02-16 17:06:14 -0600

    Add files via upload

---
+++ ejercicio4.py
@@ -0,0 +1,30 @@
+#Promedio  de 4 notas
+
+#Encabezado
+print("-------------------------------")
+print("----- PROMEDIO DE 4 NOTAS -----")
+print("-------------------------------")
+#Escriba su nombre 
+nombre=input ("Escribe tu nombre")
+print ("Escribe tu nombre", nombre)
+
+#Menu
+p = input('''
+"MENU"
+1. Promedio uno
+2. Promedio dos
+3. Promedio tres
+4. Promedio cuatro 
+5. Salir
+
+Seleccione una opcion:''') 
+
+if p == '1':
+    print("Ingrese el promedio dos, tres, cuatro")
+    print("Divida el resultado de las sumas de los promeduios anteriores")
+
+
+
+
+
+
