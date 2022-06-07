bec6f926f94cfcbfac393e31d412be549a7766d5
RaulPX11 <99362109+RaulPX11@users.noreply.github.com>
2022-06-07 13:40:04 -0600

    EJER.2_C++
    C++_EJER.2

---
+++ tarea2_datatype.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    int alumnos = 6;
+    double puntos = 97.14;
+    float pi = 3.1416;
+    long double dinero = 12.89243;
+    string nombre = "Raul";
+    char C = '67';
+    char O = '79';
+    char D = '68';
+    bool asistencia = true;
+     int claves[5] = {1,2,3,4,5}; 
+    int clv[5];
+    clv[0] = 1;
+    clv[1] = 3;
+    clv[2] = 5;
+
+    return  0;
+}
