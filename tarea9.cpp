8985cf53522c350f72f0c3bb5a5201529c83d181
RaulPX11 <99362109+RaulPX11@users.noreply.github.com>
2022-07-19 10:36:33 -0600

    Tarea 9
    Operación de números/Commit

--- tarea9.cpp
+++ tarea9.cpp
@@ -4,67 +4,39 @@
 using namespace std; 
 
 int main(void){
-    int num1 = 0;
-    int num2 = 0;
     int res = 0;
     int resultado = 0;
+    int num1 = 0;
+    int num2 = 0;
     string menu = "\t\tINGRESE LOS NUMEROS A OPERAR\n\t1. Suma\n\t2. Resta\n\t3. Multiplicacion\n\t4. Division\n\t5. Potencia\n";
     cout << menu;
+    cout << "Ingrese el primer numero: ";
+    cin >> num1;
+    cout << "Ingrese el segundo numero: ";
+    cin >> num2;
+    cout << "Ingrese la operacion que quiera realizar: ";
     cin >> res;
 
     // Sentencia Switch
-    switch (res){
+    switch(res){
         case 1: 
-        resultado = num1 + num2;
-        cout << "Ingrese el primer numero:";
-        cin >> num1;
-        cout << "Ingrese el segundo numero: ";
-        cin >> num2;
-        cout << "La suma de los numeros es: ";
-        cout << resultado << endl;
-        break;
-
-        case 2:
-         resultado = num1 - num2;
-        cout << "Ingrese el primer numero:";
-        cin >> num1;
-        cout << "Ingrese el segundo numero: ";
-        cin >> num2;
-        cout << "La resta de los numeros es: ";
-        cout << resultado << endl;
-        break;
-
-        case 3:
-         resultado = num1 * num2;
-        cout << "Ingrese el primer numero:";
-        cin >> num1;
-        cout << "Ingrese el segundo numero: ";
-        cin >> num2;
-        cout << "La multiplicacion de los numeros es: ";
-        cout << resultado << endl;
-        break;
-
-        case 4:
-         resultado = num1 / num2;
-        cout << "Ingrese el primer numero:";
-        cin >> num1;
-        cout << "Ingrese el segundo numero: ";
-        cin >> num2;
-        cout << "La division de los numeros es: ";
-        cout << resultado << endl;
-        break;
-
-        case 5:
-         resultado = pow(num1,num2);
-        cout << "Ingrese el primer numero:";
-        cin >> num1;
-        cout << "Ingrese el segundo numero: ";
-        cin >> num2;
-        cout << "La potencia de los numeros es: ";
-        cout << resultado << endl;
-        break;
-
-
+            cout << "La suma de los numeros es: " << num1 + num2 << endl;
+            break;
+        case 2: 
+            cout << "La resta de los numeros es: " << num1 - num2 << endl;    
+            break;
+        case 3: 
+            cout << "La multiplicacion de los numeros es: " << num1 * num2 << endl;
+            break;
+        case 4: 
+            cout << "La division de los numeros es: " << num1 / num2 << endl;
+            break;
+        case 5: 
+            cout << "El resultado de la potencia es: " << pow(num1,num2) << endl;
+            break;
+        default: 
+            cout << "La operacion que ingresaste no es valida" << endl;
     }
+   
 
 }
