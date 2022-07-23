66dfa894078f82045e2d0dda1a7b666c185ad9ff
RaulPX11 <99362109+RaulPX11@users.noreply.github.com>
2022-07-22 21:18:51 -0600

    Ejercicio 11

---
+++ ejercicio11.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+    cout << "Impresion de numeros pares, excepto del 50 \n"; 
+      for(int i=0; i<=100; i=i+2){
+        if( i==50){
+        cout << i << endl;
+        };
+    }
+
+
+     cout << "Impresion de numeros impares, excepto el 57 \n"; 
+      for(int i=0; i<=100; i=i+2){
+        if( i==57){
+        cout << i << endl;
+        };
+ }
+        
+    
+
