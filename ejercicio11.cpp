b0f5fe5b33f41588e7b3e72d527acb40cfd8d0d6
RaulPX11 <99362109+RaulPX11@users.noreply.github.com>
2022-07-26 19:39:37 -0600

    Ejercicio 11
    Numero pares e impares | C++

--- ejercicio11.cpp
+++ ejercicio11.cpp
@@ -2,20 +2,30 @@
 using namespace std;
 
 int main(void){
+    // ---- NUMEROS PARES ----
     cout << "Impresion de numeros pares, excepto del 50 \n"; 
       for(int i=0; i<=100; i=i+2){
-        if( i==50){
+        if( i!=50){
         cout << i << endl;
         };
-    }
+    } 
 
+    // ---- NUMEROS IMPARES ----
 
      cout << "Impresion de numeros impares, excepto el 57 \n"; 
-      for(int i=0; i<=100; i=i+2){
-        if( i==57){
+      for(int i=1; i<=100; i=i+2){
+        if( i!=57){
         cout << i << endl;
         };
  }
+}
+
+
+
+
+
+
+
         
     
 
