f2260de924ebe4f4d5890eb712a42772a0412fa4
Raúl Pérez Xoyón <99362109+RaulPX11@users.noreply.github.com>
2023-03-21 09:19:51 -0600

    TRIANGULO

---
+++ 4to. Bach/Python/main_triangulo.py
@@ -0,0 +1,22 @@
+# 1 paquete de Python
+# Modulo principal
+# Uso del as
+# Menu principal
+# Modulo secundario 
+# Funcion perimetro, area, clasificacionlado, clasificacionangulo
+import op_triangulo as op
+print('~~~~~~~~~~~ TRIANGULO ~~~~~~~~~~~')
+
+menu = input('''
+        MENU
+    1. Perimetro del Triangulo
+    2. Area del Triangulo
+    3. Tipo de Triangulo
+''')
+
+if menu == 1:
+    op.perimetro()
+elif menu == 2:
+    pass
+elif menu == 3:
+    pass
