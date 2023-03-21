62047aa37623a58c5d0f37dda4219ebf3f919c41
Raúl Pérez Xoyón <99362109+RaulPX11@users.noreply.github.com>
2023-03-21 09:18:18 -0600

    EJERCICIO TRIANGULO

---
+++ 4to. Bach/Python/op_triangulo.py
@@ -0,0 +1,8 @@
+# OPERACIONES DEL TRIANGULO
+def perimetro():
+    lado1 = float(input('Lado numero 1: '))
+    lado2 = float(input('Lado numero 2: '))
+    lado3 = float(input('Lado numero 3: '))
+    pmt_tg = lado1 + lado2 + lado3
+    print('El perimetro del triangulo es: ', pmt_tg)
+
