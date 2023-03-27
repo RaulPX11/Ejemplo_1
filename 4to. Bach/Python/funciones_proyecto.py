a76bd77b95d41ba3e7b4258aac5a723a055a07fa
Raúl Pérez Xoyón <99362109+RaulPX11@users.noreply.github.com>
2023-03-27 09:48:55 -0600

    PROYECTO RENAP

---
+++ 4to. Bach/Python/funciones_proyecto.py
@@ -0,0 +1,7 @@
+def nacimiento():
+    edad = input('Ingrese la fecha de su nacimiento: ')
+    print(edad)
+
+def dpi():
+    dpi = int(input('Ingrese el numero de su DPI: '))
+    print(dpi)
---
+++ 4to. Bach/Python/sistema_renap.py
@@ -0,0 +1,15 @@
+# Menu, registro de nacimiento, solicitud dpi, certificado de defuncion
+print('~~~~~~~~~ RENAP ~~~~~~~~~')
+
+menu = input('''
+    1. Registro de Nacimiento
+    2. Solicitud de DPI
+    3. Certificado de Defuncion
+''')
+
+if menu == '1':
+    pass
+elif menu == '2':
+    pass
+elif menu == '3':
+    pass
